ef3809737e1f9f281b368576be6405236f9c5525
mohammad <imousavi41@gmail.com>
2024-08-27 11:54:27 +0330

    Variables

---
+++ 01-variables/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var age int     // variable declaration
+	// var name string // variable declaration
+	// fmt.Println(age)
+	// fmt.Println(name)
+
+	// age = 10 //assignment
+	// fmt.Println(age)
+
+	// var age = 10            //assignment
+	// var name = "topdemy.ir" //assignment
+	// fmt.Println(age)
+	// fmt.Println(name)
+
+	// //Declaring a variable with an initial value
+
+	// var age, sum int
+	// fmt.Println(age, sum)
+	// age = 10
+	// sum = 100
+	// fmt.Println(age, sum)
+
+	// var age, sum = 10, 100
+	// fmt.Println(age, sum)
+
+	// var (
+	// 	age  = 10
+	// 	name = "mohammad"
+	// 	sum  = 1000
+	// )
+
+	// fmt.Println(age, name, sum)
+
+	// Short hand declaration
+	var age int
+	var count int
+	count, age, a := 10, 20, 30
+	fmt.Println(count, age, a)
+}
